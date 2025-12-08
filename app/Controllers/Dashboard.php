53b7f6a0e29eba8856926a7cbd2f4d71ee622721
oliber246 <sonccomachacajordan@gmail.com>
2025-12-08 09:24:10 -0500

    solución de BD 2

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -8,6 +8,17 @@ class Dashboard extends BaseController
 {
     public function index()
     {
+        // --- CÓDIGO DE REPARACIÓN TEMPORAL ---
+        $db = \Config\Database::connect();
+        try {
+            // Intentamos agregar la columna
+            $db->query("ALTER TABLE usuarios ADD COLUMN email VARCHAR(100) AFTER usuario;");
+            echo "<h1 style='color:green; text-align:center; margin-top:50px;'>¡EXITO! BASE DE DATOS REPARADA. <br> Ahora borra este código y recarga.</h1>";
+            die(); // Detenemos todo aquí para que veas el mensaje
+        } catch (\Throwable $e) {
+            // Si falla (o ya existe), seguimos normal
+        }
+        // -------------------------------------
         $productoModel = new ProductoModel();
         
         // Obtenemos todos los productos
