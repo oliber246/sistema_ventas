a9f6bef9f8fe56d1d9d32c0fa5a31d61474abce6
oliber246 <sonccomachacajordan@gmail.com>
2025-12-08 09:26:24 -0500

    solución de BD 3

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,16 +8,5 @@ class Home extends BaseController
     {
         return view('welcome_message');
     }
-    public function repararBase()
-    {
-        $db = \Config\Database::connect();
-
-        // Ejecutamos el comando SQL directamente desde PHP
-        try {
-            $db->query("ALTER TABLE usuarios ADD COLUMN email VARCHAR(100) AFTER usuario;");
-            echo "<h1 style='color:green'>¡ÉXITO! Columna 'email' agregada correctamente.</h1>";
-        } catch (\Throwable $e) {
-            echo "<h1 style='color:red'>Error o ya existe: " . $e->getMessage() . "</h1>";
-        }
-    }
+    
 }
